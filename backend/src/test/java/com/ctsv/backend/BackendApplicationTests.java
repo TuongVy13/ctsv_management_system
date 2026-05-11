4c5820ec758f194f1b40046efbe849bb34dfd6a1
Dongoctuongvy <donngoctuongvy@gmail.com>
2026-05-11 17:53:48 +0700

    initial project structure

---
+++ backend/src/test/java/com/ctsv/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctsv.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
